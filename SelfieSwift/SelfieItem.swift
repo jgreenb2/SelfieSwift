d24b4c80abb263043ff8a30e2eb7eae3d102ce97
jgreenb2 <jgreenb2@ford.com>
2015-07-24 21:52:30 -0700

    initialization and labeling for SelfieItem

--- SelfieSwift/SelfieItem.swift
+++ SelfieSwift/SelfieItem.swift
@@ -5,29 +5,87 @@
 //  Created by Jeff Greenberg on 7/24/15.
 //  Copyright © 2015 Jeff Greenberg. All rights reserved.
 //
-//  SelfieItem is a non-UI class to hold all data related to a Selfie
+//  SelfieItem holds all data related to a Selfie
+//
+// UIKit is only needed to manage the imageData associated with the Selfie
 //
 import Foundation
+import UIKit
 
 class SelfieItem {
-    private var label:String
     private let photoPath:String
     private let thumbPath:String
+    private let thumbImage:UIImage?
+    private let isChecked = false
     
-    init(fileName:String, photoPath:String, thumbHeight: Int, thumbWidth: Int) {
-        self.photoPath = photoPath
-        
+    private struct SelfieConstants {
+        static let CacheSubDir = "selfieThumb"
+        static let JpegQuality:CGFloat = 0.75
+    }
+   
+    init(fileName:String, fullSizsePhotoPath:String, thumbHeight: Int, thumbWidth: Int) {
+        photoPath = fullSizsePhotoPath
+        thumbPath = SelfieItem.getThumbPath(fullSizsePhotoPath)
+        thumbImage = SelfieItem.newThumb(targetH: thumbHeight, targetW: thumbWidth,
+                                         imageJPEGPath: fullSizsePhotoPath, thumbPath: thumbPath)
     }
     
-    private func newThumb(targetH:Int, targetW:Int, imageJPEGPath:String) -> NSData {
-        let fileManager = NSFileManager()
-        let cacheUrl: NSURL?
-        do {
-            cacheUrl = try fileManager.URLForDirectory(.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
-        } catch _ {
-            print("couldn't get cache dir")
+    private var label:String {
+        get {
+            let defaults = NSUserDefaults.standardUserDefaults()
+            if let storedLabel = defaults.stringForKey(photoPath.lastPathComponent) {
+                return storedLabel
+            } else {
+                return photoPath.lastPathComponent
+            }
         }
-        
+        set {
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setValue(newValue, forKey: photoPath.lastPathComponent)
+        }
     }
     
+    
+    private class func getThumbPath(imagePath: String) -> String {
+        let fileManager = NSFileManager()
+        let cacheUrl = try! fileManager.URLForDirectory(.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
+        let cachePath = cacheUrl.path! + "/" + SelfieConstants.CacheSubDir
+        
+        // if the thumbNail subdirectory doesn't exist, create it
+        if !fileManager.fileExistsAtPath(cachePath) {
+            try! fileManager.createDirectoryAtPath(cachePath, withIntermediateDirectories: true, attributes: nil)
+        }
+        // if the thumbNail exists, just read it from the cache
+        // otherwise create one
+        return cachePath+"/"+imagePath.lastPathComponent
+    }
+    
+    private class func newThumb(targetH targetH:Int, targetW:Int, imageJPEGPath:String, thumbPath:String) -> UIImage? {
+        let thumbImage:UIImage?
+        let fileManager = NSFileManager()
+        if fileManager.fileExistsAtPath(thumbPath) {
+            thumbImage = UIImage(contentsOfFile: thumbPath)
+        } else {
+            if let originalImage = UIImage(contentsOfFile: imageJPEGPath) {
+                let scaleFactor = min(originalImage.size.width/CGFloat(targetW), originalImage.size.height/CGFloat(targetH))
+                let newSize = CGSize(width: originalImage.size.width*scaleFactor, height: originalImage.size.height*scaleFactor)
+                thumbImage = imageWithImage(originalImage, scaledToSize: newSize)
+                if let jpegData = UIImageJPEGRepresentation(thumbImage!, SelfieConstants.JpegQuality) {
+                    jpegData.writeToFile(thumbPath, atomically: true)
+                }
+            } else {
+                thumbImage = nil
+            }
+        }
+        return thumbImage
+    }
+    
+}
+
+func imageWithImage(image:UIImage, scaledToSize newSize:CGSize) -> UIImage {
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+    image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
+    let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return newImage
 }
