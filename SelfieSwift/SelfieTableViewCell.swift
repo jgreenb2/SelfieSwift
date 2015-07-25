c013c4f4973e8cb972852187f8c00e011342f395
jgreenb2 <jgreenb2@ford.com>
2015-07-25 08:29:08 -0700

    adding table view cells

---
+++ SelfieSwift/SelfieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelfieTableViewCell.swift
+//  SelfieSwift
+//
+//  Created by Jeff Greenberg on 7/25/15.
+//  Copyright © 2015 Jeff Greenberg. All rights reserved.
+//
+
+import UIKit
+
+class SelfieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
